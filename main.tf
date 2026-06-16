cee7fdd9f385a38fb3469179462263d832e1a8c0
successe468-code <successe468@gmail.com>
2026-06-16 11:12:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
   block_public_acls       = false
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
